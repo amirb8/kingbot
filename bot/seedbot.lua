a435ccee9ab8caabc3be5fbc9168e5ac1dfe25f8
amirb8 <bagheriamir009@gmail.com>
2016-05-16 22:53:29 +0100

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = { 132855224,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -515,3 +515,5 @@ our_id = 0
 now = os.time()
 math.randomseed(now)
 started = false
+,
+    Loading plugin
